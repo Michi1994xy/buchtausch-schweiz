d93dba30b7394fb38271357468e16e95bb3bb99d
Michi1994xy <widmer.michael94@hotmail.com>
2026-04-09 16:51:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ h// ============================================================
 // ============================================================
 
 const SUPABASE_URL = 'https://ppklhjglbhmrcervmpsr.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIhnJlZiI6InBwa2xoamdsYmhtcmNlcnZtcHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjYxMzMsImV4cCI6MjA5MTMwMjEzM30.GcbSa1GvxOc6YrUlLIpN1zxCW3HR-ZiGf1JB1Sa8peE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwa2xoamdsYmhtcmNlcnZtcHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjYxMzMsImV4cCI6MjA5MTMwMjEzM30.GcbSa1GvxOc6YrUlLIpN1zxCW3HR-ZiGf1JB1Sa8peE';
 
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
